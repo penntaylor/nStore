3277d28546d9f690fda5a5ddf58c9c46badf6ce1
Joshua Koch <jkoch@efcsystems.com>
2019-10-24 22:41:32 +0000

    Initial cut at setup.py build

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ from setuptools import setup
 __version__ = '0.0.1'
 
 setup(
-  name = 'nstore',
+  name = 'nStore',
   version = __version__,
+  description = '',
+  long_description = '',
   author = 'Penn Taylor',
   author_email = 'rpenn3@gmail.com',
   url = 'https://github.com/penntaylor/nStore.git',
-  description = '',
-  long_description = '',
+  license='ALv2',
+  packages = [ 'nstore' ],
   install_requires = [
-    'boto3' ] )
+    'boto3' ],
+  include_package_data = True )
