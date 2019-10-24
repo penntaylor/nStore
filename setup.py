1d62bd34f3adcf8e07709b3c0fc9aec4dd32ea75
Joshua Koch <jkoch@efcsystems.com>
2019-10-24 20:09:27 +0000

    Add start of setup.py script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+__version__ = '0.0.1'
+
+setup(
+  name = 'nstore',
+  version = __version__,
+  author = 'Penn Taylor',
+  author_email = 'rpenn3@gmail.com',
+  url = 'https://github.com/penntaylor/nStore.git',
+  description = '',
+  long_description = '',
+  install_requires = [
+    'boto3' ] )
